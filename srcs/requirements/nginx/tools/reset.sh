c6a07dc7f5e1f3259a06bfb583dcdbeb50a7a4bb
debian <houaslam>
2024-01-09 13:55:54 +0100

    Inception

--- srcs/requirements/nginx/tools/reset.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo rm -rf /var/www/test.com/index.html
-# sudo rm -rf /etc/nginx/sites-available/default
-# sudo rm -rf /etc/nginx/sites-enabled/default
-sudo unlink /etc/nginx/sites-enabled/default
-
--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,38 +1,21 @@
 #!/bin/bash
 
-sudo touch /var/www/test.com/index.html
-sudo bash -c 'echo " 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>My web page</title>
-    </head>
-    <body>
-        <h1>Hello, world!</h1>
-        <p>This is my first web page.</p>
-        <p>It contains a 
-             <strong>main heading</strong> and <em> paragraph </em>.
-        </p>
-    </body>
-</html>" > /var/www/test.com/index.html'
+echo "hello from the nginx container"
+bash -c 'echo "
+server {
+	listen 80;
+	listen [::]:80;
 
-# sudo bash -c 'echo "
-# server {
-	# listen 80;
-	# listen [::]:80;
+	server_name test.com;
 
-	# server_name test.com;
+	root /var/www/test.com;
+	index index.html;
 
-	# root /var/www/test.com;
-	# index index.html;
+	location / {
+		try_files $uri $uri/ =404;
+	}
+}" > /etc/nginx/sites-available/default'
 
-	# location / {
-	# 	try_files $uri $uri/ =404;
-	# }
-# }" > /etc/nginx/sites-available/default'
-
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled
-sudo systemctl reload nginx
-# sudo rm -rf /etc/nginx/sites-enabled/default
-
-
+ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled
+nginx -s reload
+nginx -g "daemon off;"
