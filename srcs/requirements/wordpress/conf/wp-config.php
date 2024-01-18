cb74924d5cb97ae26f31aa8a8fc5132b13a52824
Hajar Ouaslam <houaslam@e2r8p8.1337.ma>
2024-01-18 21:49:05 +0100

    Inception

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,12 +1,12 @@
 <?php
 
-define( 'DB_NAME', 'mariadb');
+define( 'DB_NAME', getenv('WORDPRESS_DB_NAME'));
 
-define( 'DB_USER', 'MyWordPressUser');
+define( 'DB_USER', getenv('WORDPRESS_DB_USER'));
 
-define( 'DB_PASSWORD', 'Pa$$5w0rD' );
+define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
 
-define( 'DB_HOST', 'mariadb' );
+define( 'DB_HOST', getenv('WORDPRESS_DB_HOST'));
 
 define( 'DB_CHARSET', 'utf8' );
 
