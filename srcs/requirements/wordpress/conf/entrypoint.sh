068118564e883fa033fb71125568e77eaa0532d2
Hajar Ouaslam <houaslam@e2r8p8.1337.ma>
2024-01-22 11:16:38 +0100

    Inception

---
+++ srcs/requirements/wordpress/conf/entrypoint.sh
@@ -0,0 +1,5 @@
+# wp core download --allow-root
+# sleep 2
+# wp config create --dbname=mariadb --dbuser=mariadbuser --dbpass=password --dbhost=mariadb --dbprefix=wp_ --allow-root
+sleep 2
+wp core install --url=houaslam.42.fr --allow-root --title=houaslam --admin_user=houaslam --admin_password=password --admin_email=houaslam@gmail.gmail.com
--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -2,7 +2,7 @@
 define( 'DB_NAME', 'mariadb' );
 define( 'DB_USER', 'mariadbuser' );
 define( 'DB_PASSWORD', 'password' );
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mariadb' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 define( 'AUTH_KEY',         'mettez une phrase unique ici' );
