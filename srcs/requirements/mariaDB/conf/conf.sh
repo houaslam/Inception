1373a731e8af3613755c20fd0f5cc55a8593e973
houaslam <miraihajar16@gmail.com>
2024-01-25 14:26:55 +0100

    Inception

---
+++ srcs/requirements/mariaDB/conf/conf.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+RUN service mariadb start \
+    && sleep 5 \
+    && mysql -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;" \
+    && mysql -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'localhost' IDENTIFIED BY '$MARIADB_PASSWORD';" \
+    && mysql -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';" \
+    && mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" \
+    && mysql -e "FLUSH PRIVILEGES;" \
+    && service mariadb stop
+
+mysqld_safe
--- srcs/requirements/wordpress/conf/entrypoint.sh
+++ srcs/requirements/wordpress/conf/entrypoint.sh
@@ -2,28 +2,53 @@
 
 file_path="/var/www/wordpress/wp-config.php"
 cd /var/www/wordpress
+# if [ -f "$file_path" ]; then
+#     echo "WordPress directory already exists."
+# else
+#     wp core download --allow-root 
+#     wp config create --dbname=mariadb \
+#     --dbuser=mariadbuser \
+#     --dbpass=password \
+#     --dbhost=mariadb \
+#     --dbprefix=wp_ \
+#     --allow-root
+# fi
+# sleep 2
+# wp core install --url=houaslam.42.fr \
+#     --allow-root \
+#     --title=houaslam \
+#     --admin_user=houaslam \
+#     --admin_password=password \
+#     --admin_email=houaslam@gmail.com
+# sleep 2
+# wp user create hajar editor@gmail.com \
+#  --role=editor \
+#  --user_pass=editor \
+#  --allow-root
+
 if [ -f "$file_path" ]; then
     echo "WordPress directory already exists."
 else
     wp core download --allow-root 
-    wp config create --dbname=mariadb \
-    --dbuser=mariadbuser \
-    --dbpass=password \
-    --dbhost=mariadb \
+    wp config create --dbname=$MARIADB_DATABASE \
+    --dbuser=$MARIADB_USER \
+    --dbpass=$MARIADB_PASSWORD \
+    --dbhost=$MARIADB_HOST \
     --dbprefix=wp_ \
     --allow-root
 fi
 sleep 2
-wp core install --url=houaslam.42.fr \
+wp core install --url=$DOMAIN_NAME \
     --allow-root \
-    --title=houaslam \
-    --admin_user=houaslam \
-    --admin_password=password \
-    --admin_email=houaslam@gmail.com
+    --title=$WB_TITLE \
+    --admin_user=$ADMIN_USER \
+    --admin_password=$ADMIN_PASSWORD \
+    --admin_email=$ADMIN_EMAIL
 sleep 2
-wp user create hajar editor@gmail.com \
- --role=editor \
- --user_pass=editor \
+wp user create $USER $USER_EMAIL \
+ --role=$USER_ROLE \
+ --user_pass=$USER_PASS \
  --allow-root
+
 
 /usr/sbin/php-fpm7.4 -F
