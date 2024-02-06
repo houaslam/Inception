65fdbbdb3be88b27d60f69c26f7de8f0269c6f82
houaslam <miraihajar16@gmail.com>
2024-02-06 01:57:05 +0100

    Inception

--- srcs/requirements/mariaDB/conf/conf.sh
+++ srcs/requirements/mariaDB/conf/conf.sh
@@ -4,8 +4,9 @@ service mariadb start \
     && mysql -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;" \
     && mysql -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'localhost' IDENTIFIED BY '$MARIADB_PASSWORD';" \
     && mysql -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';" \
-    && mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" \
+    && mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';" \
     && mysql -e "FLUSH PRIVILEGES;" \
     && service mariadb stop
 
+mysqladmin -u root -p$MARIADB_ROOT_PASSWORD shutdown
 mysqld_safe
